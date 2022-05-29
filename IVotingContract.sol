f58e6e20ddc2d973dbc99be11d522a49500d8bd7
ANJOLAOLUWA OLOWOKERE <102741405+AN-JOLA@users.noreply.github.com>
2022-05-29 22:51:08 +0100

    Update IVotingContract.sol

--- IVotingContract.sol
+++ IVotingContract.sol
@@ -2,7 +2,27 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
+contract IVotingContract {
+   
+    struct Voter {
+        uint weight; 
+        bool voted; 
+        address delegate; 
+        uint vote; 
+    }
 
+    struct Candidates {
+        bytes32 name;   // short name
+        uint voteCount; // number of accumulated votes
+    }
+
+    address public chairperson; //person allowed to add candidates
+
+    mapping(address => Voter) public voters;
+
+    Candidates[] public candidates; //array of candidates created by chairperson
+    
+    
 interface IVotingContract{
 
 //only one address should be able to add candidates
